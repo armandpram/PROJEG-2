78afbb4f44df534951a51ab6261a160325adc60c
armandpram <armandpram@gmail.com>
2020-03-12 17:54:28 +0700

    armand ganteng

---
+++ PROJEG DUA/index COBA.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PROJEG DUA</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <div>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">EVENT</a></li>
+        <li><a href="#">LOGIN</a></li>
+        <li><a href="#">CONTACT US</a></li>
+        </div>
+    </header>    
+</body>
+</html>
---
+++ PROJEG DUA/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir=" ltr">
+    <head>
+    <meta charset="utf-8">
+    <title>Projeg Dua</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="localhost://PROJEG DUA/"></script>
+    </head>
+<body>
+    <input type="checkbox" id="check">
+    <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-time" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+        <header>MENU</header>
+        <ul>
+            <li><a href="#"><i class="fas fa-qrqode"></i>Dashboard</a></li>
+            <li><a href="#"><i class="fas fa-link"></i>Login Me</a></li>
+            <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
+            <li><a href="#"><i class="fas fa-calendar-month"></i>Event</a></li>
+            <li><a href="#"><i class="fas fa-question-circle"></i>About Me</a></li>
+        </ul>
+    </div>
+</body>
+</html>    
